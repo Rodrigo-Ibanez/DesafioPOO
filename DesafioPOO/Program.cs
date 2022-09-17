cdaba08b12d588e88c367a335dd1a0b45f46e966
Rodrigo ibanez <107516493+Rodrigo-Ibanez@users.noreply.github.com>
2022-09-17 04:17:11 -0300

    Add files via upload

---
+++ DesafioPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia N = new Nokia(numero: "12378258", modelo: "nokia1", imei: "1111",memoria: 64);
+
+N.Ligar();
+N.InstalarAplicativo("facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone I = new Iphone(numero: "98745632", modelo: "iphone1", imei: "2222",memoria: 254);
+
+I.ReceberLigacao();
+I.InstalarAplicativo("whatsapp");
+
